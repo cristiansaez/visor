cristiansaez/visor

commit af690613d873a5aeaf9ad0cd1f55f23b813c1d67

Deprecate WatchInstanceStart

instance.go
@@ -10,7 +10,6 @@ import (
 	"path"
 	"sort"
 	"strconv"
-	"strings"
 	"time"
 
 	cp "github.com/soundcloud/cotterpin"
@@ -921,32 +920,15 @@ func (s *Store) GetLostInstances() ([]*Instance, error) {
 
 // WatchInstanceStart sends Instance over the given listener channel which
 // transitioned to start.
+//
+// DEPRECATED: This method is deprecated. WatchEvent should be used directly.
 func (s *Store) WatchInstanceStart(listener chan *Instance, errors chan error) {
-	sp := s.GetSnapshot()
-	for {
-		ev, err := sp.Wait(path.Join(instancesPath, "*", registeredPath))
-		if err != nil {
-			errors <- err
-			return
-		}
-		sp = sp.Join(ev)
-
-		if !ev.IsSet() {
-			continue
-		}
-		idstr := strings.Split(ev.Path, "/")[2]
-
-		id, err := parseInstanceID(idstr)
-		if err != nil {
-			errors <- err
-			return
-		}
-		ins, err := getInstance(id, ev.GetSnapshot())
-		if err != nil {
-			errors <- err
-			return
-		}
-		listener <- ins
+	eventc := make(chan *Event)
+	go func() {
+		listener <- (<-eventc).Source.(*Instance)
+	}()
+	if err := s.WatchEvent(eventc, EvInsReg); err != nil {
+		errors <- err
 	}
 }
 

commit 41c6e563662bc3fbf6130493331c21c4f4c613c1

Fix WatchInstanceStart for unclaimed instances
The method WatchInstanceStart method handles previously claimed tickets
again. This change also fixes a regression bug introduced in af69061,
where WatchInstanceStart would only handle a single event and block
afterwards.

instance.go
@@ -925,9 +925,11 @@ func (s *Store) GetLostInstances() ([]*Instance, error) {
 func (s *Store) WatchInstanceStart(listener chan *Instance, errors chan error) {
 	eventc := make(chan *Event)
 	go func() {
-		listener <- (<-eventc).Source.(*Instance)
+		for {
+			listener <- (<-eventc).Source.(*Instance)
+		}
 	}()
-	if err := s.WatchEvent(eventc, EvInsReg); err != nil {
+	if err := s.WatchEvent(eventc, EvInsReg, EvInsUnclaim); err != nil {
 		errors <- err
 	}
 }

instance_test.go
@@ -465,25 +465,25 @@ func TestWatchInstanceStartAndStop(t *testing.T) {
 		t.Error(err)
 	}
 
-	select {
-	case ins = <-l:
-		// TODO Check other fields
-		if ins.AppName == app && ins.RevisionName == rev && ins.ProcessName == proc {
-			break
-		}
-		t.Errorf("received unexpected instance: %s", ins.String())
-	case err := <-errc:
+	if err := expectInstance(ins, l, errc); err != nil {
 		t.Fatal(err)
-	case <-time.After(time.Second):
-		t.Errorf("expected instance, got timeout")
 	}
 
-	ins, err = ins.Claim("10.0.0.1")
-	if err != nil {
+	if _, err = ins.Claim("10.0.0.99"); err != nil {
 		t.Fatal(err)
 	}
-	ins, err = ins.Started("10.0.0.1", "localhost", 5555, 5556)
-	if err != nil {
+	if _, err = ins.Unclaim("10.0.0.99"); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := expectInstance(ins, l, errc); err != nil {
+		t.Fatal(err)
+	}
+
+	if _, err = ins.Claim("10.0.0.1"); err != nil {
+		t.Fatal(err)
+	}
+	if _, err = ins.Started("10.0.0.1", "localhost", 5555, 5556); err != nil {
 		t.Fatal(err)
 	}
 
@@ -699,5 +699,20 @@ func testInstanceStatus(s *Store, t *testing.T, id int64, status InsStatus) {
 	}
 	if ins.Status != status {
 		t.Errorf("expected instance status to be '%s' got '%s'", status, ins.Status)
+	}
+}
+
+func expectInstance(i *Instance, instancec chan *Instance, errc chan error) error {
+	select {
+	case ins := <-instancec:
+		// TODO Check other fields
+		if ins.AppName == i.AppName && ins.RevisionName == i.RevisionName && ins.ProcessName == i.ProcessName {
+			return nil
+		}
+		return fmt.Errorf("received unexpected instance: %s", ins.String())
+	case err := <-errc:
+		return err
+	case <-time.After(time.Second):
+		return errors.New("expected instance, got timeout")
 	}
 }