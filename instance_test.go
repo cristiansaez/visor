cristiansaez/visor

commit 1fa25bc5d774b6425d55d7b59cc1701e028e890b

Fix WatchEvent for old events
This fixes a bug during event enrichment for an instance which has been
unregistered in the meantime. As some clients might lack behind in the
timeline and process events for old instances, it's important that the
Snapshot gets never been forwarded during event enrichment. A test has
been added to ensure the correct behavior.

instance.go
@@ -692,13 +692,7 @@ func (i *Instance) claimed(ip string) {
 func (i *Instance) getRestarts() (InsRestarts, *cp.File, error) {
 	var restarts InsRestarts
 
-	sp, err := i.GetSnapshot().FastForward()
-	if err != nil {
-		return restarts, nil, err
-	}
-	i.dir = i.dir.Join(sp)
-
-	f, err := sp.GetFile(i.dir.Prefix(restartsPath), new(cp.ListIntCodec))
+	f, err := i.dir.GetFile(i.dir.Prefix(restartsPath), new(cp.ListIntCodec))
 	if err == nil {
 		fields := f.Value.([]int)
 
@@ -928,17 +922,16 @@ func (s *Store) GetLostInstances() ([]*Instance, error) {
 // WatchInstanceStart sends Instance over the given listener channel which
 // transitioned to start.
 func (s *Store) WatchInstanceStart(listener chan *Instance, errors chan error) {
-	// instances/*/start =
 	sp := s.GetSnapshot()
 	for {
-		ev, err := sp.Wait(path.Join(instancesPath, "*", startPath))
+		ev, err := sp.Wait(path.Join(instancesPath, "*", registeredPath))
 		if err != nil {
 			errors <- err
 			return
 		}
 		sp = sp.Join(ev)
 
-		if !ev.IsSet() || string(ev.Body) != "" {
+		if !ev.IsSet() {
 			continue
 		}
 		idstr := strings.Split(ev.Path, "/")[2]

instance_test.go
@@ -367,13 +367,6 @@ func TestInstanceRestarted(t *testing.T) {
 	if ins2.Restarts.Fail != 2 {
 		t.Error("expected restart count to be set to 2")
 	}
-	ins3, err := storeFromSnapshotable(ins).GetInstance(ins.ID)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if ins3.Restarts.Fail != 2 {
-		t.Error("expected restart count to be set to 2")
-	}
 
 	err = ins1.Unregister("test-bot", fmt.Errorf("cleanup"))
 	if err != nil {
@@ -463,8 +456,9 @@ func TestWatchInstanceStartAndStop(t *testing.T) {
 	env := "w-env"
 	s := instanceSetup()
 	l := make(chan *Instance)
+	errc := make(chan error)
 
-	go s.WatchInstanceStart(l, make(chan error))
+	go s.WatchInstanceStart(l, errc)
 
 	ins, err := s.RegisterInstance(app, rev, proc, env)
 	if err != nil {
@@ -478,6 +472,8 @@ func TestWatchInstanceStartAndStop(t *testing.T) {
 			break
 		}
 		t.Errorf("received unexpected instance: %s", ins.String())
+	case err := <-errc:
+		t.Fatal(err)
 	case <-time.After(time.Second):
 		t.Errorf("expected instance, got timeout")
 	}

event_test.go
@@ -40,18 +40,18 @@ func eventAppSetup(s *Store, name string) *App {
 	return s.NewApp(name, "git://"+name, name+"stack")
 }
 
-func expectEvent(etype EventType, s cp.Snapshotable, l chan *Event, t *testing.T) (event *Event) {
+func expectEvent(etype EventType, s cp.Snapshotable, l chan *Event, t *testing.T) *Event {
 	for {
 		select {
-		case event = <-l:
+		case event := <-l:
 			if event.Type == etype {
 				if reflect.TypeOf(event.Source) != reflect.TypeOf(s) {
-					t.Errorf("types are not equal %#v != %#v", event.Source, s)
+					t.Errorf("%s types are not equal: %#v != %#v", etype, event.Source, s)
 				}
 			} else {
 				t.Errorf("received incorrect event type: expected %s got %s %s", etype, event, event.Type)
 			}
-			return
+			return event
 		case <-time.After(time.Second):
 			t.Fatalf("expected event type %s got timeout", etype)
 		}
@@ -346,4 +346,25 @@ func TestEventInstanceStateChange(t *testing.T) {
 		t.Error(err)
 	}
 	expectEvent(EvInsExit, ins, l, t)
+}
+
+func TestEventInstanceEnrichment(t *testing.T) {
+	s, l := eventSetup()
+
+	ins, err := s.RegisterInstance("foo", "bar", "baz", "qux")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := ins.Unregister("common-host", errors.New("exited")); err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		if err := s.WatchEvent(l); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	expectEvent(EvInsReg, ins, l, t)
+	expectEvent(EvInsUnreg, nil, l, t)
 }

commit 578bbd39e1db3d379b1ca47613d590895cb86643

Fix getting the restart count.
This bug was introduced in 1fa25bc. The `dir` already handles the
prefix.

Add a test case to check actually getting the restart count from the
storage.

instance.go
@@ -692,7 +692,7 @@ func (i *Instance) claimed(ip string) {
 func (i *Instance) getRestarts() (InsRestarts, *cp.File, error) {
 	var restarts InsRestarts
 
-	f, err := i.dir.GetFile(i.dir.Prefix(restartsPath), new(cp.ListIntCodec))
+	f, err := i.dir.GetFile(restartsPath, new(cp.ListIntCodec))
 	if err == nil {
 		fields := f.Value.([]int)
 

instance_test.go
@@ -379,6 +379,32 @@ func TestInstanceRestarted(t *testing.T) {
 	}
 }
 
+func TestInstanceRestartAndGet(t *testing.T) {
+
+	s := instanceSetup()
+	ip := "10.0.0.1"
+	ins := instanceSetupClaimed("fat-pat", ip)
+
+	ins, err := ins.Started(ip, "fat-pat.com", 9999, 10000)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = ins.Restarted(InsRestarts{0, 1})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ins1, err := s.GetInstance(ins.ID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ins1.Restarts.Fail != 1 {
+		t.Errorf("restarts: got %v, want 1", ins1.Restarts.Fail)
+	}
+}
+
 func TestInstanceFailed(t *testing.T) {
 	ip := "10.0.0.1"
 	ins := instanceSetupClaimed("fat-cat", ip)