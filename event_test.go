cristiansaez/visor

commit a360fc86ee0d414d9f686db0f98baea3eb1b1d88

Consolidate event implementation

event.go
@@ -15,7 +15,13 @@ import (
 	cp "github.com/soundcloud/cotterpin"
 )
 
-const charPat = `[-.[:alnum:]]`
+// Event represents a change to a file in the registry.
+type Event struct {
+	Type   EventType // Type of event
+	Path   EventData // Unique part of the event path
+	Source cp.Snapshotable
+	raw    cp.Event // Original event returned by cotterpin
+}
 
 // EventData is used to represent information encoded in the file path.
 type EventData struct {
@@ -23,7 +29,6 @@ type EventData struct {
 	Instance *string
 	Proc     *string
 	Revision *string
-	Service  *string
 }
 
 func (d EventData) String() string {
@@ -39,16 +44,6 @@ func (d EventData) String() string {
 	}
 
 	return fmt.Sprintf("EventData{%s}", strings.Join(fields, ", "))
-}
-
-// An Event represents a change to a file in the registry.
-type Event struct {
-	Type   EventType // Type of event
-	Body   string    // Body of the changed file
-	Source cp.Snapshotable
-	Path   EventData
-	raw    *cp.Event // Original event returned by cotterpin
-	Rev    int64
 }
 
 // EventType is the used to distinguish events.
@@ -73,10 +68,6 @@ const (
 	EvUnknown   = EventType("UNKNOWN")
 )
 
-const (
-	globPlural = "**"
-)
-
 type eventPath int
 
 const (
@@ -88,6 +79,11 @@ const (
 	pathInsStatus
 	pathInsStart
 	pathInsStop
+)
+
+const (
+	charPat    = `[-.[:alnum:]]`
+	globPlural = "**"
 )
 
 var eventPatterns = map[*regexp.Regexp]eventPath{
@@ -117,7 +113,7 @@ func (s *Store) WatchEvent(listener chan *Event) error {
 		}
 		sp = sp.Join(ev)
 
-		event, err := enrichEvent(&ev, ev)
+		event, err := newEvent(ev)
 		if err != nil {
 			return err
 		}
@@ -130,169 +126,118 @@ func (s *Store) WatchEvent(listener chan *Event) error {
 	}
 }
 
-func canonicalizeMetadata(etype EventType, uncanonicalized EventData, s cp.Snapshotable) (source cp.Snapshotable, err error) {
-	var (
-		app  *App
-		rev  *Revision
-		proc *Proc
-		ins  *Instance
-	)
-
-	if uncanonicalized.App != nil {
-		app, err = getApp(*uncanonicalized.App, s)
-
-		if err != nil {
-			return
-		}
+func newEvent(src cp.Event) (*Event, error) {
+	event := &Event{
+		Type: EvUnknown,
+		raw:  src,
 	}
-
-	if uncanonicalized.Revision != nil {
-		rev, err = getRevision(app, *uncanonicalized.Revision, s)
-
-		if err != nil {
-			return
-		}
-	}
-
-	if uncanonicalized.Proc != nil {
-		proc, err = getProc(app, *uncanonicalized.Proc, s)
-		if err != nil {
-			return
-		}
-	}
-
-	if uncanonicalized.Instance != nil {
-		var id int64 = -1
-		if id, err = strconv.ParseInt(*uncanonicalized.Instance, 10, 64); err != nil {
-			return
-		}
-		if ins, err = getInstance(id, s); err != nil {
-			return
-		}
-	}
-
-	switch etype {
-	case EvAppReg:
-		source = app
-	case EvRevReg:
-		source = rev
-	case EvProcReg, EvProcAttrs:
-		source = proc
-	case EvInsReg, EvInsStart, EvInsStop, EvInsFail, EvInsExit, EvInsLost:
-		source = ins
-	}
-
-	return
-}
-
-func enrichEvent(src *cp.Event, s cp.Snapshotable) (event *Event, err error) {
-	var (
-		path            = src.Path
-		etype           = EvUnknown
-		uncanonicalized = EventData{}
-
-		canonicalized cp.Snapshotable
-	)
 
 	for re, ev := range eventPatterns {
-		if match := re.FindStringSubmatch(path); match != nil {
+		if match := re.FindStringSubmatch(src.Path); match != nil {
 			switch ev {
 			case pathApp:
-				uncanonicalized.App = &match[1]
-
 				if src.IsSet() {
-					etype = EvAppReg
+					event.Type = EvAppReg
 				} else if src.IsDel() {
-					etype = EvAppUnreg
+					event.Type = EvAppUnreg
 				}
+				event.Path = EventData{App: &match[1]}
 			case pathRev:
-				uncanonicalized.App = &match[1]
-				uncanonicalized.Revision = &match[2]
-
 				if src.IsSet() {
-					etype = EvRevReg
+					event.Type = EvRevReg
 				} else if src.IsDel() {
-					etype = EvRevUnreg
+					event.Type = EvRevUnreg
 				}
+				event.Path = EventData{App: &match[1], Revision: &match[2]}
 			case pathProc:
-				uncanonicalized.App = &match[1]
-				uncanonicalized.Proc = &match[2]
-
 				if src.IsSet() {
-					etype = EvProcReg
+					event.Type = EvProcReg
 				} else if src.IsDel() {
-					etype = EvProcUnreg
+					event.Type = EvProcUnreg
 				}
+				event.Path = EventData{App: &match[1], Proc: &match[2]}
 			case pathProcAttrs:
-				uncanonicalized.App = &match[1]
-				uncanonicalized.Proc = &match[2]
-
-				if src.IsSet() {
-					etype = EvProcAttrs
-				}
-			case pathInsRegistered:
-				uncanonicalized.Instance = &match[1]
-
-				if src.IsSet() {
-					etype = EvInsReg
-				} else if src.IsDel() {
-					etype = EvInsUnreg
-				}
-			case pathInsStart:
-				body := string(src.Body)
-
-				if !src.IsSet() || body == "" {
+				if !src.IsSet() {
 					break
 				}
-
-				uncanonicalized.Instance = &match[1]
-
-				fields := strings.Fields(body)
-				if len(fields) > 1 {
-					etype = EvInsStart
+				event.Type = EvProcAttrs
+				event.Path = EventData{App: &match[1], Proc: &match[2]}
+			case pathInsRegistered:
+				if src.IsSet() {
+					event.Type = EvInsReg
+				} else if src.IsDel() {
+					event.Type = EvInsUnreg
 				}
+				event.Path = EventData{Instance: &match[1]}
+			case pathInsStart:
+				if !src.IsSet() || len(src.Body) == 0 {
+					break
+				}
+				event.Type = EvInsStart
+				event.Path = EventData{Instance: &match[1]}
 			case pathInsStop:
 				if !src.IsSet() {
 					break
 				}
-
-				uncanonicalized.Instance = &match[1]
-				etype = EvInsStop
+				event.Type = EvInsStop
+				event.Path = EventData{Instance: &match[1]}
 			case pathInsStatus:
-				uncanonicalized.Instance = &match[1]
-
 				if !src.IsSet() {
 					break
 				}
-
 				switch InsStatus(src.Body) {
 				case InsStatusRunning:
-					etype = EvInsStart
+					event.Type = EvInsStart
 				case InsStatusExited:
-					etype = EvInsExit
+					event.Type = EvInsExit
 				case InsStatusFailed:
-					etype = EvInsFail
+					event.Type = EvInsFail
 				case InsStatusLost:
-					etype = EvInsLost
+					event.Type = EvInsLost
 				}
+				event.Path = EventData{Instance: &match[1]}
 			}
 			break
 		}
 	}
 
-	if etype != EvUnknown && src.IsSet() {
-		canonicalized, err = canonicalizeMetadata(etype, uncanonicalized, s)
+	if event.Type != EvUnknown && src.IsSet() {
+		var err error
+		event.Source, err = getSource(event.Type, event.Path, src)
 		if err != nil {
 			return nil, fmt.Errorf("error canonicalizing inputs %+v: %s", src, err)
 		}
 	}
 
-	return &Event{
-		Type:   etype,
-		Body:   string(src.Body),
-		Source: canonicalized,
-		Path:   uncanonicalized,
-		raw:    src,
-		Rev:    src.Rev,
-	}, nil
+	return event, nil
+}
+
+func getSource(etype EventType, path EventData, s cp.Snapshotable) (cp.Snapshotable, error) {
+	var (
+		app *App
+		err error
+	)
+
+	if path.App != nil {
+		app, err = getApp(*path.App, s)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	switch etype {
+	case EvAppReg:
+		return app, nil
+	case EvRevReg:
+		return getRevision(app, *path.Revision, s)
+	case EvProcReg, EvProcAttrs:
+		return getProc(app, *path.Proc, s)
+	case EvInsReg, EvInsStart, EvInsStop, EvInsFail, EvInsExit, EvInsLost:
+		id, err := strconv.ParseInt(*path.Instance, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		return getInstance(id, s)
+	}
+	return nil, nil
 }

commit e71788dd89548b6e07ceefca3b8ea8ec519b8dfe

Fix EvInsStart event emission
The consolidation in a360fc8 introduced a regression as EvInsStart
events were now also sent when an instance got claimed. This change
reverts the behavior and adds additional tests.

event.go
@@ -6,6 +6,7 @@
 package visor
 
 import (
+	"bytes"
 	"fmt"
 	"reflect"
 	"regexp"
@@ -173,7 +174,7 @@ func newEvent(src cp.Event) (*Event, error) {
 				}
 				event.Path = EventData{Instance: &match[1]}
 			case pathInsStart:
-				if !src.IsSet() || len(src.Body) == 0 {
+				if !src.IsSet() || len(bytes.Fields(src.Body)) < 2 {
 					break
 				}
 				event.Type = EvInsStart

event_test.go
@@ -313,12 +313,12 @@ func TestEventInstanceStateChange(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	ins, err = ins.Claim(ip)
-	if err != nil {
-		t.Fatal(err)
-	}
 
 	go storeFromSnapshotable(ins).WatchEvent(l)
+
+	if _, err = ins.Claim(ip); err != nil {
+		t.Fatal(err)
+	}
 
 	ins, err = ins.Started(ip, host, port, tPort)
 	if err != nil {
@@ -328,7 +328,6 @@ func TestEventInstanceStateChange(t *testing.T) {
 	if ev.Path.Instance == nil || (*ev.Path.Instance != strconv.FormatInt(ins.ID, 10)) {
 		t.Error("event.Path doesn't contain expected data")
 	}
-
 	instance := ev.Source.(*Instance)
 	if instance.IP != ip || instance.Host != host || instance.Port != port {
 		t.Fatal("instance fields don't match")
@@ -384,6 +383,12 @@ func TestEventFilter(t *testing.T) {
 		t.Fatal(err)
 	}
 	if _, err = ins.Unclaim("1.2.3.4"); err != nil {
+		t.Fatal(err)
+	}
+	if _, err = ins.Claim("1.2.8.9"); err != nil {
+		t.Fatal(err)
+	}
+	if _, err = ins.Started("1.2.8.9", "host.com", 9090, 9095); err != nil {
 		t.Fatal(err)
 	}
 	if err := ins.Unregister("common-host", errors.New("exited")); err != nil {